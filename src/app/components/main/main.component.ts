a3a7c31ee6fd7ef5f3ced41fb1512b93db4660de
oscar <oscarvicente@usal.es>
2021-05-11 15:11:48 +0200

    Main Component v0.1

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  speed: String;
+  altitude: String;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.speed = '2438 m/s';
+    this.altitude = '6,5 Gm';
   }
 
 }
