e27cfa7802f663a4b11cb8a5b88ad9242e4af1d3
oscar <oscarvicente@usal.es>
2021-06-05 20:17:40 +0200

    v1.0, firebase deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -43,7 +42,7 @@ import { GunnerCountermeasuresComponent } from './components/gunner/gunner-count
     ComanderComsComponent,
     ComanderAirlockCameraComponent,
     ComanderNavComponent,
-    ComanderNavVesselComponent
+    ComanderNavVesselComponent,
     PilotDroneControlComponent,
     PilotNavControlComponent,
     PilotDockingComponent,
