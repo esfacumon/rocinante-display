f0b0f682cd61d7afe1187badb411a8b4b05b0684
oscar <oscarvicente@usal.es>
2021-06-20 13:16:33 +0200

    Comander-nav: añadido borde a la imagen del mapa para simular una pantalla 							Aumento margin superior
    Comander-main: añadido borde a la imagen central para simular una pantalla
    
    Comander-airlock: añadido borde a la imagen central para simular una pantalla
    
    Comander-combat: añadido borde a la imagen del mapa para simular una pantalla
    
    Pilot-nav: añadido borde a la imagen del mapa para simular una pantalla
    							Aumento margin superior
    							Implementados botones cambio valores orbita

--- src/app/components/pilot/pilot-nav-control/pilot-nav-control.component.ts
+++ src/app/components/pilot/pilot-nav-control/pilot-nav-control.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PilotNavControlComponent implements OnInit {
 
+  speed : number = 2000;
+  heading : number = 64;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  incSpeed(){
+    this.speed = this.speed + 10;
+  }
+
+  decSpeed(){
+    this.speed = this.speed - 10;
+  }
+
+  incHeading(){
+    this.heading = this.heading + 1;
+  }
+
+  decHeading(){
+    this.heading = this.heading - 1;
+  }
 }
